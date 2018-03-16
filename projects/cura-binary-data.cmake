b32bfef374060ccfdbc67e07e80d3cd9e843f03e
Anthony Markowitz <SunPSoftware>
2018-03-16 16:58:34 -0400

    first commit

--- projects/cura-binary-data.cmake
+++ projects/cura-binary-data.cmake
@@ -1,6 +1,6 @@
 
 ExternalProject_Add(cura-binary-data
-    GIT_REPOSITORY https://github.com/ultimaker/cura-binary-data
+    GIT_REPOSITORY https://github.com/SunPSoftware/cura-binary-data-3-2
     GIT_TAG origin/${CURABINARYDATA_BRANCH_OR_TAG}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
 )
--- projects/fdm_materials.cmake
+++ projects/fdm_materials.cmake
@@ -1,6 +1,6 @@
 
 ExternalProject_Add(fdm_materials
-    GIT_REPOSITORY https://github.com/ultimaker/fdm_materials
+    GIT_REPOSITORY https://github.com/SunPSoftware/fdm_materials-3-2
     GIT_TAG origin/${FDMMATERIALS_BRANCH_OR_TAG}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
 )
